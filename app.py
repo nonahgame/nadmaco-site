55d029a7c15880c23b16eba8e4db8da86dc39b91
nonahgame <103342324+nonahgame@users.noreply.github.com>
2024-08-01 20:03:34 +0000

    added apps

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__, template_folder='template')
+
+@app.route("/")
+def template():
+    return render_template("home.html")
+
+JOBS = [
+        {
+        "id": 1,
+        "title": "Data Scintist",
+        "location": "Nigeria",
+        "salary": "NGN 50000"
+    },
+    {
+        "id": 2,
+        "title": "Front-Backend Engineer",
+        "location": "S-Africa, Liberia"
+    },
+    {
+    "id": 3,
+    "title": "Data Analyst",
+    "location": "US, UK",
+    "salary": "$, 200"
+    }
+]
+
+@app.route("/page1")
+def pages():
+    return render_template('home.html',jobs=JOBS)
+
+@app.route("/api")
+def pagejs():
+    return jsonify(JOBS)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
